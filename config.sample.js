15801590c41704bce44c8787d5de2b76795c034e
Johannes Rudolph <johannes.rudolph@gmx.com>
2016-10-20 20:51:54 +0200

    extend example conf

--- config.sample.js
+++ config.sample.js
@@ -1,10 +1,23 @@
 var config = {};
 
 config.ldap = {};
-
 config.ldap.server = "ldap://ldap.example.com";
 config.ldap.port = 389;
 config.ldap.bindUser = 'cn=admin,dc=example,dc=com';
 config.ldap.bindPW = 'example';
+
+config.smtp = {};
+config.smtp.user = "example@example.com";
+config.smtp.pw = "example";
+config.smtp.server = "mail.example.com";
+config.smtp.sender.name = "Example Name";
+config.smtp.sender.email = "do-not-replay@example.com";
+
+config.user = {};
+config.user.passwordsalt = "exampleSalt";
+config.user.password.salt = "ljasdhjkahsd";
+config.user.password.tokenVaildTimeInMinutes = 30;
+config.user.password.forgetLink = "http://www.example.com/passwordforget/?token=:token";
+config.user.password.forgetPlainText = "Hello :name, bla :tokenVaildTimeInMinutes bla :link bla";
 
 module.exports = config;
