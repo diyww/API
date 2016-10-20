381bef0c97c863f3b6f7f74206d9ad8aa755876a
Johannes Rudolph <johannes.rudolph@gmx.com>
2016-10-20 20:50:20 +0200

    routes/users: add password forget

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var config = require('../config');
+var mail = require('../modules/mail');
+var user = require('../modules/user');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+/* POST Password forget */
+router.post('/passwordforget', function(req, res, next) {
+  var email = req.body.email;
+
+  var token = user.createPasswordToken(email);
+  var link = config.user.password.forgetLink.replace(":token",token);
+  var plaintext = config.user.password.forgetPlainText.replace(":link",link).replace(":name","HORST");
+
+  mail.sendMail(email,"PW",plaintext,"<b>"+plaintext+"</b>");
+
+  res.send(email);
+  console.log(req.body.email);
 });
 
 module.exports = router;
