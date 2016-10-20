976577869d19b53e7c779e0a8c2f7555557b7a97
Johannes Rudolph <johannes.rudolph@gmx.com>
2016-10-20 20:49:26 +0200

    routes/rfid: use ldap module

--- routes/rfid.js
+++ routes/rfid.js
@@ -1,50 +1,15 @@
-var ldap = require('ldapjs');
 var express = require('express');
 var router = express.Router();
 var config = require('../config');
 var tokens = require('../tokens');
-var _ = require('underscore');
-
-var client = ldap.createClient({
-    url: config.ldap.server+":"+config.ldap.port
-});
-client.bind(config.ldap.bindUser, config.ldap.bindPW, function(err) {
-    //assert.ifError(err);
-});
+var ldap = require('../modules/ldap');
 
 /* GET User for rfid String. */
 router.get('/:id', function(req, res, next) {
-
-
     if (tokens.indexOf(req.headers.token) > -1) {
-
-        console.log(req.headers.token);
-
-        var opts = {
-            filter: '(employeeNumber='+req.params.id+')',
-            scope: 'sub'
-        };
-
-        client.search('ou=Mitglieder,dc=diyww,dc=de', opts, function(err, result) {
-            //assert.ifError(err);
-            var resultFound = false;
-            result.on('searchEntry', function(entry) {
-                resultFound = true;
-                var data = entry.object;
-                res.send(_.omit(data, ['userPassword']));
-            });
-            result.on('searchReference', function(referral) {
-                res.send('referral: ' + referral.uris.join());
-            });
-            result.on('error', function(err) {
-                res.send('error: ' + err.message);
-            });
-
-            result.on('end', function(result) {
-                if(!resultFound){
-                    res.send({"error":"No Result"});
-                }
-            });
+        var result = res;
+        ldap.findRfidCard(req.params.id,function (err, res) {
+            result.send(res);
         });
     } else {
         res.send({"error":"Invalid Token"});
